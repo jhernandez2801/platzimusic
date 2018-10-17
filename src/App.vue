f014a3bafd7bddb9d92c383929a3a4e1155a224b
Jorge Hernandez <jhernandez@tecnologias101.com>
2018-10-17 12:47:18 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 {{ msg }}
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" :value="country.value") {{country.name}}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid") 
+    </template>
+
+<script>
+import getArtists from './api'
+import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'PlatziMusic',
+      artists: [],
+      countries: [
+        {name: 'Argentina', value: 'Argentina'},
+        {name: 'Colombia', value: 'Colombia'},
+        {name: 'España', value: 'Spain'},
+      ],
+      selectedCountry: 'argentina',
+      loading: true,
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  methods:{
+    refreshArtists(){
+      const self=this
+      this.loading=true
+      this.artists=[]
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          self.loading=false
+          self.artists = artists
+        })
+    }
+  },
+  mounted(){
+    this.refreshArtists()    
+  },
+  watch: {
+    selectedCountry: function(){
+      this.refreshArtists()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app 
+	font-family 'Avenir', Helvetica, Arial, sans-serif
+	-webkit-font-smoothing antialiased
+	-moz-osx-font-smoothing grayscale
+	text-align center
+	color red !important
+	margin-top 60px
+h1, h2 
+	font-weight normal
+ul 
+	list-style-type none
+	padding 0
+li 
+	display inline-block
+	margin 0 10px
+a 
+	color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default{
+    appName: 'platzimusic',
+    apiKey: '6c9b164bb0940f16635b13fa726ca163',
+    secret: '27358c25a13ba691748b74c04d760a3a',
+    registeredTo: 'jhernandez2801'
+}
